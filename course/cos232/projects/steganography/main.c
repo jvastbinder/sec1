1087c70c37f7ba9dc259114fa02cbd27df2f1d6a
joel <joel_vastbinder@taylor.edu>
2017-09-09 13:56:32 -0400

    Milestone 1 commit: file parses command line args, opens files, and stores message.

--- course/cos232/projects/steganography/main.c
+++ course/cos232/projects/steganography/main.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
 int main(int argc, char *argv[]) {
+    FILE *input, *output;
+    char *message = argv[3];
+    input = fopen(argv[1], "r");
+    output = fopen(argv[2], "w");
     return 0;
 }
