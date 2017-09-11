e9ae0ec422ec6701c0b2f536164ba5545db06c1f
jvastbin <joel_vastbinder@taylor.edu>
2017-09-11 16:45:41 -0400

    Finds start of pixel array successfully

--- course/cos232/projects/steganography/main.c
+++ course/cos232/projects/steganography/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
-int main(int argc, char *argv[]) {
-    FILE *input, *output;
+int main(int argc, char *argv[])
+{
+    /*
+     * bitmap file header
+     * 0: 1 byte file type
+     * 1: 2 byte file size
+     * 3: 1 byte reserved
+     * 4: 1 byte reserved
+     * 5: 2 byte offset to start of pixel array
+     */
+    FILE *inImage, *outImage;
+    int startOfPixelArray[1];
+
+    inImage       = fopen("sheldon.bmp", "r");
+    outImage      = fopen("enc.bmp", "w");
     char *message = argv[3];
-    input = fopen(argv[1], "r");
-    output = fopen(argv[2], "w");
+
+    fseek(inImage, 10, SEEK_SET);
+    fread(startOfPixelArray, 4, 1, inImage);
+    printf("Pixels start at: %d\n", startOfPixelArray[0]);
+
+    fclose(inImage);
+    fclose(outImage);
+
     return 0;
 }
